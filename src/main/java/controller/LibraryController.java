afbe587822eac00128bd1caa04caf2400d166031
ajongs <35219960+ajongs@users.noreply.github.com>
2021-07-16 14:28:33 +0900

    Library 컨트롤러 생성

--- src/main/java/controller/LibraryController.java
+++ src/main/java/controller/LibraryController.java
@@ -1,2 +1,26 @@
-package controller;public class LibraryController {
+package controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import service.LibraryService;
+
+@Controller
+public class LibraryController {
+
+    @Autowired
+    LibraryService libraryService;
+
+    @ResponseBody
+    @RequestMapping(value="/book/{title}", method= RequestMethod.GET)
+    public ResponseEntity searchBook(@PathVariable String title){
+        return new ResponseEntity(libraryService.searchBook(title), HttpStatus.OK);
+    }
+
 }
