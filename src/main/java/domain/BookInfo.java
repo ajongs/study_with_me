6729f3394033e80716f69f30a29cdefd22d3ff17
ajongs <35219960+ajongs@users.noreply.github.com>
2021-07-16 14:29:21 +0900

    bookInfo 도메인 생성

--- src/main/java/domain/BookInfo.java
+++ src/main/java/domain/BookInfo.java
@@ -1,2 +1,202 @@
-package domain;public class BookInfo {
+package domain;
+
+public class BookInfo {
+    private String titleInfo;
+    private String typeName;
+    private String placeInfo;
+    private String authorInfo;
+    private String menuName;
+    private String mediaName;
+    private String manageName;
+    private String pubYearInfo;
+    private String controlNo;
+    private String docYn;
+    private String orgLink;
+    private String id;
+    private String typeCode;
+    private String licYn;
+    private String licText;
+    private String regDate;
+    private String detailLink;
+    private String isbn;
+    private String calNo;
+    private String kdcCode1s;
+    private String kdcName1s;
+    private String classNo;
+
+    public String getTitleInfo() {
+        return titleInfo;
+    }
+
+    public void setTitleInfo(String titleInfo) {
+        this.titleInfo = titleInfo;
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+
+    public String getPlaceInfo() {
+        return placeInfo;
+    }
+
+    public void setPlaceInfo(String placeInfo) {
+        this.placeInfo = placeInfo;
+    }
+
+    public String getAuthorInfo() {
+        return authorInfo;
+    }
+
+    public void setAuthorInfo(String authorInfo) {
+        this.authorInfo = authorInfo;
+    }
+
+    public String getMenuName() {
+        return menuName;
+    }
+
+    public void setMenuName(String menuName) {
+        this.menuName = menuName;
+    }
+
+    public String getMediaName() {
+        return mediaName;
+    }
+
+    public void setMediaName(String mediaName) {
+        this.mediaName = mediaName;
+    }
+
+    public String getManageName() {
+        return manageName;
+    }
+
+    public void setManageName(String manageName) {
+        this.manageName = manageName;
+    }
+
+    public String getPubYearInfo() {
+        return pubYearInfo;
+    }
+
+    public void setPubYearInfo(String pubYearInfo) {
+        this.pubYearInfo = pubYearInfo;
+    }
+
+    public String getControlNo() {
+        return controlNo;
+    }
+
+    public void setControlNo(String controlNo) {
+        this.controlNo = controlNo;
+    }
+
+    public String getDocYn() {
+        return docYn;
+    }
+
+    public void setDocYn(String docYn) {
+        this.docYn = docYn;
+    }
+
+    public String getOrgLink() {
+        return orgLink;
+    }
+
+    public void setOrgLink(String orgLink) {
+        this.orgLink = orgLink;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTypeCode() {
+        return typeCode;
+    }
+
+    public void setTypeCode(String typeCode) {
+        this.typeCode = typeCode;
+    }
+
+    public String getLicYn() {
+        return licYn;
+    }
+
+    public void setLicYn(String licYn) {
+        this.licYn = licYn;
+    }
+
+    public String getLicText() {
+        return licText;
+    }
+
+    public void setLicText(String licText) {
+        this.licText = licText;
+    }
+
+    public String getRegDate() {
+        return regDate;
+    }
+
+    public void setRegDate(String regDate) {
+        this.regDate = regDate;
+    }
+
+    public String getDetailLink() {
+        return detailLink;
+    }
+
+    public void setDetailLink(String detailLink) {
+        this.detailLink = detailLink;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getCalNo() {
+        return calNo;
+    }
+
+    public void setCalNo(String calNo) {
+        this.calNo = calNo;
+    }
+
+    public String getKdcCode1s() {
+        return kdcCode1s;
+    }
+
+    public void setKdcCode1s(String kdcCode1s) {
+        this.kdcCode1s = kdcCode1s;
+    }
+
+    public String getKdcName1s() {
+        return kdcName1s;
+    }
+
+    public void setKdcName1s(String kdcName1s) {
+        this.kdcName1s = kdcName1s;
+    }
+
+    public String getClassNo() {
+        return classNo;
+    }
+
+    public void setClassNo(String classNo) {
+        this.classNo = classNo;
+    }
 }
